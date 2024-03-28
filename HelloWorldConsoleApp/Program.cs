9c641345e676d2ac4c7c1c7c1470db9d24cfdf80
Mikita Kalashnikau1 <mikita_kalashnikau1@epam.com>
2024-03-28 16:47:23 +0300

    Module 3 solution

---
+++ HelloWorldConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Please provide a username.");
+            return;
+        }
+
+        string userName = args[0];
+        string message = HelloWorldHelper.GenerateMessage(userName);
+        Console.WriteLine(message);
+    }
+}
---
+++ HelloWorldWPFApp/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorldWPFApp
+{
+    public partial class MainWindow : Window
+    {
+        public string Username { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void textBoxUsername_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                string username = textBoxUsername.Text;
+                string message = HelloWorldHelper.GenerateMessage(username);
+
+                MessageBox.Show(message, "Hello Message");
+
+                // Clear the TextBox for next input
+                textBoxUsername.Clear();
+            }
+        }
+    }
+}
