0e98c0d2a016046f321fbbf1d36f6295e1ac5e71
Mikita Kalashnikau1 <mikita_kalashnikau1@epam.com>
2024-04-04 18:57:09 +0300

    Fixed unit tests

--- Task1/Product.cs
+++ Task1/Product.cs
@@ -11,5 +11,13 @@
         public string Name { get; set; }
 
         public double Price { get; set; }
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            var other = (Product)obj;
+            return Name == other.Name && Price == other.Price;
+        }
     }
 }
