a07b8dcc538a32f4c5a332919627b6423a116b27
Mikita Kalashnikau1 <mikita_kalashnikau1@epam.com>
2024-04-28 21:13:04 +0300

    Task 2

--- FileSystemVisitor.cs
+++ FileSystemVisitor.cs
@@ -4,58 +4,128 @@ using System.IO;
 
 public class FileSystemVisitor
 {
-    private readonly string _rootFolder;
+    public event EventHandler<FileSystemInfoEventArgs> Start;
+    public event EventHandler<FileSystemInfoEventArgs> Finish;
+    public event EventHandler<FileSystemInfoEventArgs> FileFound;
+    public event EventHandler<FileSystemInfoEventArgs> DirectoryFound;
+    public event EventHandler<FileSystemInfoEventArgs> FilteredFileFound;
+    public event EventHandler<FileSystemInfoEventArgs> FilteredDirectoryFound;
+
+    private readonly string _rootDirectory;
     private readonly Predicate<string> _filter;
 
-    public FileSystemVisitor(string rootFolder, Predicate<string> filter = null)
+    public FileSystemVisitor(string rootDirectory, Predicate<string> filter = null)
     {
-        _rootFolder = rootFolder;
+        _rootDirectory = rootDirectory;
         _filter = filter;
     }
 
     public IEnumerable<string> Traverse()
     {
-        Stack<string> stack = new Stack<string>();
-        stack.Push(_rootFolder);
+        OnStart(new FileSystemInfoEventArgs(_rootDirectory));
 
-        while (stack.Count > 0)
+        foreach (var item in SafeEnumerateFileSystemEntries(_rootDirectory, "*", SearchOption.AllDirectories))
         {
-            string currentDir = stack.Pop();
+            bool isFile = File.Exists(item);
+            bool isDirectory = Directory.Exists(item);
+            bool isMatched = _filter?.Invoke(item) ?? false;
 
-            bool shouldReturnCurrentDir = _filter == null || _filter(currentDir);
+            var args = new FileSystemInfoEventArgs(item, isMatched);
 
-            if (shouldReturnCurrentDir)
+            if (isFile)
             {
-                yield return currentDir;
+                FileFound?.Invoke(this, args);
+
+                if (isMatched)
+                {
+                    FilteredFileFound?.Invoke(this, args);
+                }
+            }
+            else if (isDirectory)
+            {
+                DirectoryFound?.Invoke(this, args);
+
+                if (isMatched)
+                {
+                    FilteredDirectoryFound?.Invoke(this, args);
+                }
             }
 
-            string[] directories = null;
-            string[] files = null;
+            if (args.Action == ActionType.Abort)
+            {
+                yield break;
+            }
+            else if (args.Action == ActionType.Exclude && isMatched || _filter != null && !isMatched)
+            {
+                continue;
+            }
 
+            yield return item;
+        }
+
+        OnFinish(new FileSystemInfoEventArgs(_rootDirectory));
+    }
+
+    private IEnumerable<string> SafeEnumerateFileSystemEntries(
+        string rootDirectory, string searchPattern, SearchOption searchOptions)
+    {
+        var dirsToEnumerate = new Queue<string>();
+        dirsToEnumerate.Enqueue(rootDirectory);
+
+        while (dirsToEnumerate.Count > 0)
+        {
+            var currentDir = dirsToEnumerate.Dequeue();
+            IEnumerable<string> entries;
             try
             {
-                directories = Directory.GetDirectories(currentDir);
-                files = Directory.GetFiles(currentDir);
+                entries = Directory.EnumerateFileSystemEntries(currentDir, searchPattern);
             }
             catch (UnauthorizedAccessException)
             {
                 continue;
             }
 
-            foreach (var directory in directories)
+            foreach (var entry in entries)
             {
-                stack.Push(directory);
-            }
-
-            foreach (var file in files)
-            {
-                bool shouldReturnFile = _filter == null || _filter(file);
-
-                if (shouldReturnFile)
+                yield return entry;
+                if (Directory.Exists(entry) && searchOptions == SearchOption.AllDirectories)
                 {
-                    yield return file;
+                    dirsToEnumerate.Enqueue(entry);
                 }
             }
         }
+    }
+
+    protected virtual void OnStart(FileSystemInfoEventArgs e)
+    {
+        EventHandler<FileSystemInfoEventArgs> handler = Start;
+        handler?.Invoke(this, e);
+    }
+
+    protected virtual void OnFinish(FileSystemInfoEventArgs e)
+    {
+        EventHandler<FileSystemInfoEventArgs> handler = Finish;
+        handler?.Invoke(this, e);
+    }
+}
+
+public enum ActionType
+{
+    Continue,
+    Exclude,
+    Abort
+}
+
+public class FileSystemInfoEventArgs : EventArgs
+{
+    public string Path { get; }
+    public ActionType Action { get; set; }
+    public bool IsMatched { get; }
+
+    public FileSystemInfoEventArgs(string path, bool isMatched = false)
+    {
+        Path = path;
+        IsMatched = isMatched;
+        Action = ActionType.Continue;
     }
 }
