0b0a0a25548b257906a7454ebb3c845ddaf1418c
Mikita Kalashnikau1 <mikita_kalashnikau1@epam.com>
2024-06-14 17:30:00 +0300

    Http Fundamentals module

---
+++ Client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Text;
+
+class Program
+{
+    static HttpClient client = new HttpClient();
+
+    static void Main(string[] args)
+    {
+        CallMyName().GetAwaiter().GetResult();
+        GetStatusCode("Information").GetAwaiter().GetResult();
+        GetStatusCode("Success").GetAwaiter().GetResult();
+        GetStatusCode("Redirection").GetAwaiter().GetResult();
+        GetStatusCode("ClientError").GetAwaiter().GetResult();
+        GetStatusCode("ServerError").GetAwaiter().GetResult();
+
+    }
+
+    static async Task CallMyName()
+    {
+        HttpResponseMessage response = await client.GetAsync("http://localhost:8888/MyName");
+
+        if (response.IsSuccessStatusCode)
+        {
+            string content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("I am " + content);
+        }
+    }
+
+    static async Task GetStatusCode(string path)
+    {
+        HttpResponseMessage response = await client.GetAsync($"http://localhost:8888/{path}");
+
+        Console.WriteLine($"{path}:{response.StatusCode}");
+    }
+}
---
+++ Listener/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using System.Text;
+
+class Program
+{
+    static HttpListener listener;
+
+    static void Main(string[] args)
+    {
+        listener = new HttpListener();
+        listener.Prefixes.Add("http://localhost:8888/");
+        listener.Start();
+
+        Console.WriteLine("Listening...");
+
+        while (true)
+        {
+            HttpListenerContext context = listener.GetContext();
+            HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+
+            var path = request.RawUrl.Replace("/", "");
+
+            switch (path)
+            {
+                case "MyName":
+                    var name = Encoding.UTF8.GetBytes("Your name"); //Change 'Your name' to your name
+                    response.OutputStream.Write(name, 0, name.Length);
+                    break;
+                case "Information":
+                    response.StatusCode = 100;
+                    break;
+                case "Success":
+                    response.StatusCode = 200;
+                    break;
+                case "Redirection":
+                    response.StatusCode = 300;
+                    break;
+                case "ClientError":
+                    response.StatusCode = 400;
+                    break;
+                case "ServerError":
+                    response.StatusCode = 500;
+                    break;
+                default:
+                    response.StatusCode = 404;
+                    break;
+            }
+            response.Close();
+        }
+    }
+}
